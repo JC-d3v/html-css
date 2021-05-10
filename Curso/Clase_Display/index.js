ab9bc022a6727220ee840d5289738c2442007cbf
JC-d3v <calderon.rjorge@gmail.com>
2021-05-10 00:38:01 -0400

    Practica-layout

---
+++ Curso/Clase_Display/index.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {ere} nombre nbvnbvnbvnvn n nb nn
+ * @param {*} app 
+ * @param {*} apm 
+ * @param {*} data 
+ */
+
+const registra= (nombre, app, apm, data ) => {
+     console.log('.. esto se realizo con exito')
+}
+export registra
